0b124e61bb2b5f04f5794aa07d76f25fb07ebc71
herdian <herdian@lippox.com>
2017-01-27 17:12:12 +0700

    Update labeling

--- jquery.columnfilter-0.0.1.js
+++ jquery.columnfilter-0.0.1.js
@@ -34,18 +34,21 @@
                     var checked='checked';
                 }
                 if( $(this).context.nodeName == settings.headerCell ) {
-                    var checkbox = '<input type="checkbox" '+ checked +'>';
-                    columnList = $.fn.columnFilter.format( checkbox, $.trim($(this).text()) );
+                    var label = $.trim($(this).text());
+                    var checkbox = '<input type="checkbox" '+ checked +' data-label="'+ label +'">';
+                    columnList = $.fn.columnFilter.format( checkbox, label );
                     $(settings.columnCheckboxsContainer).append(columnList);
                 }
             });
         });
         $(settings.columnCheckboxsContainer).find('input').bind('click', function(){
-            var label = $(this).siblings('label').text();
+            var label = $(this).attr('data-label');
 
             var index = $(that).find('thead tr th').filter(function() {
                 return $.trim($(this).text()) === label;
             }).index();
+
+console.log(label,index);
 
             if( !$(this).is(':checked') ) {
                 localStorage.setItem(settings.localStorageNamespace + index, 'hide');
@@ -71,7 +74,6 @@
     };
 
     $.fn.columnFilter.format = function(checkbox,column) {
-        return '<label>' + checkbox + column + '</label>';
-    };
- 
+        return checkbox + '<label>' + column + '</label>';
+    }; 
 }( jQuery ));
