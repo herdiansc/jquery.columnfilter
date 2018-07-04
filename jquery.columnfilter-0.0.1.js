cf72d452fbc0a9505bb41025fed2f169427abd57
Herdian Sc <herdiansc@gmail.com>
2018-07-04 10:49:55 +0700

    Add handle from empty state row.

--- jquery.columnfilter-0.0.1.js
+++ jquery.columnfilter-0.0.1.js
@@ -27,21 +27,25 @@
         var that = this;
         var checked='';
         $(that).find('* > tr').each(function(){
-            $(this).children().each(function(index){
-                if( localStorage.getItem(settings.localStorageNamespace + index) == 'hide' ) {
-                    $(this).hide();
-                    checked='';
-                } else {
-                    $(this).show();
-                    checked='checked';
-                }
-                if( $(this).context.nodeName == settings.headerCell ) {
-                    var label = $.trim($(this).text());
-                    var checkbox = '<input type="checkbox" '+ checked +' data-label="'+ label +'">';
-                    columnList = $.fn.columnFilter.format( checkbox, label );
-                    $(settings.columnCheckboxsContainer).append(columnList);
-                }
-            });
+            // Handle empty state row
+            // We can say "Data not found" as empty state row
+            if ($(this).children().length > 1) {
+                $(this).children().each(function(index){
+                    if( localStorage.getItem(settings.localStorageNamespace + index) == 'hide' ) {
+                        $(this).hide();
+                        checked='';
+                    } else {
+                        $(this).show();
+                        checked='checked';
+                    }
+                    if( $(this).context.nodeName == settings.headerCell ) {
+                        var label = $.trim($(this).text());
+                        var checkbox = '<input type="checkbox" '+ checked +' data-label="'+ label +'">';
+                        columnList = $.fn.columnFilter.format( checkbox, label );
+                        $(settings.columnCheckboxsContainer).append(columnList);
+                    }
+                });
+            }
         });
         $(settings.columnCheckboxsContainer).find('input').bind('click', function(){
             var label = $(this).attr('data-label');
@@ -55,19 +59,25 @@
                 $(that).find('thead').children().each(function(i){
                     $(this).children().eq(index).hide();
                 });
-
-                $(that).find('tbody').children().each(function(i){
-                    $(this).children().eq(index).hide();
-                });
+                
+                // Handle empty state row
+                if ($(that).find('tbody').children().children().length > 1 && $(that).find('thead').children().children().length > 1) {
+                    $(that).find('tbody').children().each(function(i){
+                        $(this).children().eq(index).hide();
+                    });
+                }
             } else {
                 localStorage.setItem(settings.localStorageNamespace + index, 'show');
                 $(that).find('thead').children().each(function(i){
                     $(this).children().eq(index).show();
                 });
-
-                $(that).find('tbody').children().each(function(i){
-                    $(this).children().eq(index).show();
-                });
+                
+                // Handle empty state row
+                if ($(that).find('tbody').children().children().length > 1 && $(that).find('thead').children().children().length > 1) {
+                    $(that).find('tbody').children().each(function(i){
+                        $(this).children().eq(index).show();
+                    });
+                }
             }
         });
         return;
